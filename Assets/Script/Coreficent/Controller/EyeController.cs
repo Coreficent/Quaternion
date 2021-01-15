d1e572830de8d320638f1f6fdc5cc99a86400e53
Stephen Xie <coreficent@gmail.com>
2021-01-15 17:25:40 -0500

    Add debug statement for angles.

--- Assets/Script/Coreficent/Controller/EyeController.cs
+++ Assets/Script/Coreficent/Controller/EyeController.cs
@@ -37,9 +37,11 @@
 
             //transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(_target.transform.position - transform.position), _rotationSpeed * Time.deltaTime);
 
-            //DebugLogger.Log("QuaternionCurrent", QuaternionCurrent.w, QuaternionCurrent.x, QuaternionCurrent.y, QuaternionCurrent.z);
-            //DebugLogger.Log("QuaternionTransform", QuaternionTransform.w, QuaternionTransform.x, QuaternionTransform.y, QuaternionTransform.z);
-            //DebugLogger.Log("QuaternionFinal", QuaternionFinal.w, QuaternionFinal.x, QuaternionFinal.y, QuaternionFinal.z);
+            DebugLogger.Log("QuaternionCurrent", QuaternionCurrent.w, QuaternionCurrent.x, QuaternionCurrent.y, QuaternionCurrent.z);
+            DebugLogger.Log("QuaternionTransform", QuaternionTransform.w, QuaternionTransform.x, QuaternionTransform.y, QuaternionTransform.z);
+            DebugLogger.Log("QuaternionFinal", QuaternionFinal.w, QuaternionFinal.x, QuaternionFinal.y, QuaternionFinal.z);
+
+            DebugLogger.Log("Angles", QuaternionCurrent.eulerAngles);
 
             transform.rotation = Quaternion.RotateTowards(transform.rotation, QuaternionFinal, _rotationSpeed * Time.deltaTime);
         }
