cf94872373cd66fc2146625cef9163259e78967d
Stephen Xie <coreficent@gmail.com>
2021-01-17 15:35:05 -0500

    Pull.

---
+++ Assets/Script/Fang/UserInterface/ButtonController/StartMinionRotate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartMinionRotate : MonoBehaviour
+{
+
+    public Button startRotate;
+
+    public GameObject enterPage;
+
+
+    AudioSource audioSource;
+    public AudioClip impact;
+
+
+    void Start()
+    {
+        startRotate.onClick.AddListener(StartQuaternion);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void StartQuaternion()
+    {
+
+        audioSource.PlayOneShot(impact, 0.7F);
+
+        enterPage.SetActive(false);
+
+    }
+}
